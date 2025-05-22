d6b2adbab34a02cb6b43603e63a18f8382dfe502
ahmed-hisham20 <ahmedhisham16820@gmail.com>
2025-05-22 22:49:13 +0300

    update

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -5,9 +5,9 @@ export default function Error() {
     <div style={{ textAlign: 'center', padding: '60px' }}>
     <h1 style={{ fontSize: '48px', marginBottom: '20px' }}>404 - Page Not Found</h1>
     <p style={{ fontSize: '18px' }}>Sorry, the page you are looking for does not exist.</p>
-    <a href="/" style={{ marginTop: '20px', display: 'inline-block', color: '#007bff' }}>
+    <Link to="/" style={{ marginTop: '20px', display: 'inline-block', color: '#007bff' }}>
       Go back to Home
-    </a>
+    </Link>
   </div>
   )
 }
